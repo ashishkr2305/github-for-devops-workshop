94e42252df00db7af676e7c26f1cd840245e8274
ashishkr2305 <krashish9647@gmail.com>
2026-01-27 22:56:34 +0530

    gemini code

--- demo.py
+++ demo.py
@@ -1,3 +1,6 @@
+"""Module to calculate the sum of a list of numbers."""
+
 nums = [5, 10, 15, 65, 9]
 res = sum(nums)
 print(res)
+# (Make sure there is a blank line here at the very end)
--- testing.py
+++ testing.py
@@ -1,7 +1,9 @@
 
 
 def greet():
+    """Print a greeting message to the user."""
     print("Hello, World!")
 
 
 greet()
+# (Make sure there is a blank line here at the very end)
